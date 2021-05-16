f13e2234b92089617441139968b34ee5357a21b2
Alona-Suprun <a.alonka.l@gmail.com>
2021-05-16 15:54:01 +0300

    add correction

--- src/switcher.js
+++ src/switcher.js
@@ -6,27 +6,28 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const onChangeSwitcherTheme = () => {
-  body.classList.add(Theme.LIGHT);
-  if (body.classList.value === Theme.LIGHT) {
+const onChangeSwitcherTheme = e => {
+  if (e.target.checked === true) {
     body.classList.add(Theme.DARK);
     body.classList.remove(Theme.LIGHT);
   } else {
     body.classList.add(Theme.LIGHT);
     body.classList.remove(Theme.DARK);
   }
-  const bodyTheme = body.classList.value;
-  localStorage.setItem('custom-theme', bodyTheme);
+
+  localStorage.setItem('custom-theme', body.classList.value);
 };
 
 switcher.addEventListener('change', onChangeSwitcherTheme);
 
-const saveTheme = () => {
+const saveTheme = e => {
   const customTheme = localStorage.getItem('custom-theme');
   if (customTheme) {
     body.classList.add(customTheme);
-    if (body.classList.value === Theme.DARK) {
+    if (e.target.value === Theme.DARK) {
       switcher.checked = true;
+    } else {
+      body.classList.add(Theme.LIGHT);
     }
   }
 };
